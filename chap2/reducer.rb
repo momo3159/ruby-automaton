ed4dbaa01b76768d5e2faeb699caeefbe960c1e3
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-10-06 03:34:18 +0900

    Boolean/LessThanを追加

--- chap2/reducer.rb
+++ chap2/reducer.rb
@@ -12,6 +12,20 @@ class Number < Struct.new(:value)
     end
 end
 
+class Boolean < Struct.new(:value)
+    def to_s
+        value.to_s
+    end
+
+    def inspect
+        "<<#{self}>"
+    end
+
+    def reducible?
+        false
+    end
+end
+
 class Add < Struct.new(:left, :right)
     def to_s
         "#{left} + #{right}"
@@ -59,6 +73,29 @@ class Multiply < Struct.new(:left, :right)
         end
     end
 end
+
+class LessThan < Struct.new(:left, :right)
+    def to_s
+        "#{left} * #{right}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+    
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        if left.reducible?
+            LessThan.new(left.reduce(environment), right)
+        elsif right.reducible?
+            LessThan.new(left, right.reduce(environment))
+        else
+            Boolean.new(left.value < right.value)
+        end
+    end
 
 class Variable < Struct.new(:name)
     def to_s
