6c91de9fe9a86ffd2fa637aefd4e26cbd124de53
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-10-06 03:08:50 +0900

    変数に対応するクラスを追加

--- chap2/reducer.rb
+++ chap2/reducer.rb
@@ -61,6 +61,24 @@ class Multiply < Struct.new(:left, :right)
     end
 end
 
+class Variable < Struct.new(:name)
+    def to_s
+        name.to_s
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        environment[name] # nil or 変数の値
+    end
+end
+
 class Machine < Struct.new(:expression)
     def step
         self.expression = expression.reduce
