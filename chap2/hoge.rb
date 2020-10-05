1774a4d22b742b8ae8f0864349333b9e399f3bfa
momo3159 <tommydsha@gmail.com>
2020-10-06 02:57:15 +0900

    Add files via upload

---
+++ chap2/hoge.rb
@@ -0,0 +1,96 @@
+class Number < Struct.new(:value)
+    def to_s
+        value.to_s
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        false
+    end
+
+end
+
+class Add < Struct.new(:left, :right)
+    def to_s
+        "#{left} + #{right}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce
+        if left.reducible?
+            Add.new(left.reduce, right)
+        elsif right.reducible?
+            Add.new(left.value, right.reduce)
+        else
+            Number.new(left.value + right.value)
+        end
+    end
+end
+
+class Multiply < Struct.new(:left, :right)
+    def to_s
+        "#{left} * #{right}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce 
+        if left.reducible?
+            Add.new(left.reduce, right)
+        elsif right.reducible?
+            Add.new(left.value, right.reduce)
+        else
+            Number.new(left.value * right.value)
+        end
+    end
+end
+
+def reducible?(expression)
+    case expression
+    when Number
+        false
+    when Add, Multiply
+        true
+    end
+end
+
+
+class Machine < Struct.new(:expression)
+    def step
+        self.expression = expression.reduce
+    end
+
+    def run
+        while expression.reducible?
+            puts expression
+            step
+        end
+        puts expression # 最終的な結果
+    end
+end
+
+m = Machine.new(
+    # (4 * 20) + 3 という式に対応する抽象構文木
+    Add.new(
+        Multiply.new(Number.new(4), Number.new(20)),
+        Number.new(3)
+    )    
+)
+
+m.run
